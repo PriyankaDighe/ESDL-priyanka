a46847522067455d799f0070f96377687f289b8f
PriyankaDighe <PriyankaDighe@users.noreply.github.com>
2015-09-10 11:10:10 +0530

    2nd edit

--- sample.cpp
+++ sample.cpp
@@ -9,7 +9,7 @@ char name[20];
 cout<<"enter your name"<<endl;
 cin>>name;
 cout<<"welcome to github"<<endl;
-cout<<"a place where softeare is built"<<endl;
+cout<<"a place where software is built"<<endl;
 cout<<"Hello "<<name<<"!..This is a sample program"<<endl;
 return 0;
 
